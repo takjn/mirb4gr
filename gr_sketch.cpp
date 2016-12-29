377cfeb26ef68b38aa4126eb53584a0e54e10096
Jun Takeda <takeda.jun@gmail.com>
2016-12-30 01:26:33 +0900

    change tab to space

--- gr_sketch.cpp
+++ gr_sketch.cpp
@@ -146,8 +146,8 @@ void loop() {
 
     /* parse code */
     parser = mrb_parser_new(mrb);
-		if (parser == NULL) {
-			Serial.println("create parser state error");
+    if (parser == NULL) {
+      Serial.println("create parser state error");
       exit(-1);
     }
     parser->s = ruby_code;
@@ -161,22 +161,22 @@ void loop() {
       Serial.println(parser->error_buffer[0].message);
     }
     else {
-			Serial.println("Syntax OK");
+      Serial.println("Syntax OK");
     //   /* generate bytecode */
-		// 	struct RProc *proc = mrb_generate_code(mrb, parser);
-		// 	if (proc == NULL) {
-		// 		Serial.println("mrb_generate_code error");
-		// 		mrb_parser_free(parser);
-		// 		exit(-1);
-		// 	}
-		//
-		// 	/* pass a proc for evaluation */
-		// 	/* evaluate the bytecode */
-		// 	result = mrb_vm_run(mrb,
-		// 			proc,
-		// 			mrb_top_self(mrb),
-		// 			stack_keep);
-		// 	stack_keep = proc->body.irep->nlocals;
+    //   struct RProc *proc = mrb_generate_code(mrb, parser);
+    //   if (proc == NULL) {
+    //     Serial.println("mrb_generate_code error");
+    //     mrb_parser_free(parser);
+    //     exit(-1);
+    //   }
+    //
+    //   /* pass a proc for evaluation */
+    //   /* evaluate the bytecode */
+    //   result = mrb_vm_run(mrb,
+    //       proc,
+    //       mrb_top_self(mrb),
+    //       stack_keep);
+    //   stack_keep = proc->body.irep->nlocals;
     //   /* did an exception occur? */
     //   if (mrb->exc) {
     //     /* yes */
@@ -184,19 +184,19 @@ void loop() {
     //     mrb->exc = 0;
     //   }
     //   else {
-		// 		/* no */
-		// 		if (!mrb_respond_to(mrb, result, mrb_intern_lit(mrb, "inspect"))){
-		// 			result = mrb_any_to_s(mrb, result);
-		// 		}
-		// 		p(mrb, result, 1);
+    //     /* no */
+    //     if (!mrb_respond_to(mrb, result, mrb_intern_lit(mrb, "inspect"))){
+    //       result = mrb_any_to_s(mrb, result);
+    //     }
+    //     p(mrb, result, 1);
     //   }
     }
-		ruby_code[0] = '\0';
-		last_code_line[0] = '\0';
-		mrb_gc_arena_restore(mrb, ai);
+    ruby_code[0] = '\0';
+    last_code_line[0] = '\0';
+    mrb_gc_arena_restore(mrb, ai);
 
-		mrb_parser_free(parser);
-		mrb_full_gc(mrb);
-		Serial.write("> ");
+    mrb_parser_free(parser);
+    mrb_full_gc(mrb);
+    Serial.write("> ");
   }
 }
